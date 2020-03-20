479684a6f2e38a16445325e81870c173f1d30791
johanna-ohrnberg <j-ohrnberg@live.se>
2020-03-20 16:57:50 +0100

    home och about

---
+++ airbean/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <article id='home'>
+        <h1 class="title">AIR BEAN</h1>
+        <h6 class="subtitle">DRONEDELIVERED COFFEE</h6>
+    </article>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  created () {
+    setTimeout(() => this.$router.push('/about'), 2000)
+  }
+}
+</script>
+
+<style scoped>
+   #home {
+       border: 1px solid black;
+       width: 375px;
+       height: 667px;
+       margin: auto;
+       background: #38846D;
+       display: grid;
+       grid-template-columns: 32% 150px 40%;
+       grid-template-rows: 40% 10% 10% 40%;
+   }
+
+   .title {
+       color: #FFFFFF;
+       grid-column: 2 / 3;
+       grid-row: 2 / 3;
+   }
+
+   .subtitle {
+       color: #FFFFFF;
+       grid-column: 2 / 3;
+       grid-row: 3 / 4;
+   }
+</style>
