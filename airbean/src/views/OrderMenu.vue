22064f50584075ceeed23605e909ee0062a3868c
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-20 16:24:00 +0100

    Create OrderMenu.vue

---
+++ airbean/src/views/OrderMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <nav></nav>
+    <h1>Meny</h1>
+    <MenuItem />
+  </div>
+</template>
+
+<script>
+import MenuItem from '../components/MenuItem.vue'
+export default {
+  components: {
+    MenuItem
+  },
+  created() {
+    this.$store.dispatch('getMenuItems')
+    console.log(this.$store.state.menuItems.items)
+  }
+}
+</script>
+
+<style></style>
