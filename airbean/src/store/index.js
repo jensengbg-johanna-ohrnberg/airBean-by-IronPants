aaa91582621b538134106b89e77634792ffddd07
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-20 16:23:52 +0100

    Update index.js

--- airbean/src/store/index.js
+++ airbean/src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import menuItems from './modules/menuItems.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    menuItems: menuItems
   }
 })
