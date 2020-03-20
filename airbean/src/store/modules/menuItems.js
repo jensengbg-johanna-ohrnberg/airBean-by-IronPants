3912f7e435296dec33337f13b2168e0dd48f89a5
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-20 16:23:54 +0100

    Create menuItems.js

---
+++ airbean/src/store/modules/menuItems.js
@@ -0,0 +1,21 @@
+const menuItems = {
+  state: {
+    items: []
+  },
+  mutaions: {
+    sendMenuItems(state, items) {
+      state.items = items
+    }
+  },
+  actions: {
+    async getMenuItems(ctx) {
+      const url = 'http://localhost:5000/api/beans'
+      const res = await fetch(url, { method: 'GET' })
+      const data = await res.json()
+
+      ctx.commit('sendMenuItems', data)
+    }
+  }
+}
+
+export default menuItems
