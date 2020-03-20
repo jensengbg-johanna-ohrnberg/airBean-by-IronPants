d2242e5baecee25e778d8ce259b10d54c693c127
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-20 16:23:50 +0100

    Update index.js

--- airbean/src/router/index.js
+++ airbean/src/router/index.js
@@ -1,27 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Menu from "../views/OrderMenu.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     component: About
+  },
+  {
+    path: "/menu",
+    name: "Menu",
+    component: Menu
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
