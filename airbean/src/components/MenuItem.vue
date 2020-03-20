43dd38ce486738e30dc088fda9d1abaa3f412a0f
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-20 16:23:47 +0100

    Create MenuItem.vue

---
+++ airbean/src/components/MenuItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="meunItem">
+    <button class="addBtn">+</button>
+    <article>
+      <p>Bryggd på månadens bönor</p>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  props: {
+    prod: Object
+  }
+}
+</script>
+
+<style></style>
