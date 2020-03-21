227a95047836287b9944d8eebf5891b6d0e7c23d
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-21 11:15:16 +0100

    Update OrderMenu.vue

--- airbean/src/views/OrderMenu.vue
+++ airbean/src/views/OrderMenu.vue
@@ -2,21 +2,37 @@
   <div class="wrapper">
     <nav></nav>
     <h1>Meny</h1>
-    <MenuItem />
+    <MenuList />
   </div>
 </template>
 
 <script>
-import MenuItem from '../components/MenuItem.vue'
+import MenuList from '../components/MenuList.vue'
 export default {
   components: {
-    MenuItem
-  },
-  created() {
-    this.$store.dispatch('getMenuItems')
-    console.log(this.$store.state.menuItems.items)
+    MenuList
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background: #f3e4e1;
+
+  nav {
+    width: 100vw;
+    height: 6rem;
+  }
+
+  h1 {
+    margin: 1.4rem 0rem;
+    font-size: 2.6rem;
+  }
+}
+</style>
