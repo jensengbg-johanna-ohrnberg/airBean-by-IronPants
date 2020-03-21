531c32683d8ce0aad8ed348f08e0441d1a58cd14
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-21 11:15:07 +0100

    Update MenuItem.vue

--- airbean/src/components/MenuItem.vue
+++ airbean/src/components/MenuItem.vue
@@ -1,9 +1,14 @@
 <template>
-  <section class="meunItem">
+  <section class="menuItem">
     <button class="addBtn">+</button>
     <article>
-      <p>Bryggd på månadens bönor</p>
+      <p>
+        <b>{{ prod.title }}</b
+        >...........
+      </p>
+      <p class="underText">Bryggd på månadens bönor</p>
     </article>
+    <h3 class="price">{{ prod.price }} kr</h3>
   </section>
 </template>
 
@@ -16,4 +21,43 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.menuItem {
+  width: 100vw;
+  height: 5rem;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  article {
+    p {
+      b {
+        font-size: 1.4rem;
+      }
+    }
+
+    .underText {
+      font-family: Arial, Helvetica, sans-serif;
+      margin-top: 0.3rem;
+      color: #444;
+      font-size: 0.7rem;
+    }
+  }
+
+  .price {
+    margin-left: auto;
+  }
+
+  .addBtn {
+    padding: 0.2rem 0.5rem;
+    font-size: 1.4rem;
+    border-radius: 50%;
+    border: none;
+    background: #000;
+    color: #fff;
+    margin-right: 1rem;
+    font-weight: 100;
+  }
+}
+</style>
