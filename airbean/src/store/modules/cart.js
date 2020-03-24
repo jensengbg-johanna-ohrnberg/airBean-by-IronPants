3885b9979daa66dcfbc328129e252ce70d9672a5
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-24 16:27:20 +0100

    Update cart.js

--- airbean/src/store/modules/cart.js
+++ airbean/src/store/modules/cart.js
@@ -4,12 +4,38 @@ const cart = {
   },
   mutations: {
     sendProdToCart(state, prod) {
-      state.cart.push(prod)
+      if (state.cart.indexOf(prod) === -1) {
+        state.cart.push(prod)
+      }
+    },
+    prodQuantPlusOne(state, prod) {
+      for (let i = 0; i < state.cart.length; i++) {
+        if (state.cart[i].id === prod.id) {
+          state.cart[i].quant += 1
+        }
+      }
+    },
+    prodQuantMinusOne(state, prod) {
+      for (let i = 0; i < state.cart.length; i++) {
+        if (state.cart[i].id === prod.id) {
+          if (state.cart[i].quant === 1) {
+            state.cart.splice(i, 1)
+          } else {
+            state.cart[i].quant -= 1
+          }
+        }
+      }
     }
   },
   actions: {
     sendProdToCart(ctx, prod) {
       ctx.commit('sendProdToCart', prod)
+    },
+    increaseQuant(ctx, prod) {
+      ctx.commit('prodQuantPlusOne', prod)
+    },
+    decreaseQuant(ctx, prod) {
+      ctx.commit('prodQuantMinusOne', prod)
     }
   }
 }
