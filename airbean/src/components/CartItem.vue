df669512fe8ea98a121060ae757f5d5b8aeae35d
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-24 12:14:56 +0100

    Create CartItem.vue

---
+++ airbean/src/components/CartItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="cartItem">
+    <article class="prodInfo">
+      <h3>{{ prod.title }}</h3>
+      <p class="price">{{ prod.price }} kr</p>
+    </article>
+    <article>
+      <button class="arrow up"></button>
+      <input type="number" min="1" max="5" value="1" class="quant" />
+      <button class="arrow up"></button>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CartItem',
+  props: {
+    prod: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cartItem {
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: 1rem;
+  margin: 0.6rem 0rem;
+  .prodInfo {
+    height: 100%;
+    margin-right: auto;
+  }
+  input[type='number'] {
+    border: none;
+    text-align: center;
+    align-self: center;
+    padding: 0rem 0rem 0.8rem 0rem;
+  }
+
+  input[type='number']::-webkit-inner-spin-button,
+  input[type='number']::-webkit-outer-spin-button {
+    background: red;
+    padding: 1rem;
+  }
+
+  .price {
+    font-family: 'Work Sans', sans-serif;
+  }
+}
+</style>
