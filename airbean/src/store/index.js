cbfbba2cd101b6e2abe0c2ec0f57bacde9d22cc4
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-24 12:15:04 +0100

    Update index.js

--- airbean/src/store/index.js
+++ airbean/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import menuItems from './modules/menuItems.js'
+import cart from './modules/cart.js'
 
 Vue.use(Vuex)
 
@@ -9,6 +10,7 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: {
-    menuItems: menuItems
+    menuItems: menuItems,
+    cart: cart
   }
 })
