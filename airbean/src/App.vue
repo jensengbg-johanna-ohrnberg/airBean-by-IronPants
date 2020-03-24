3ec7050eb65196f6af066e52e4fb72226de704cd
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-24 12:14:48 +0100

    Update App.vue

--- airbean/src/App.vue
+++ airbean/src/App.vue
@@ -9,9 +9,17 @@ export default {}
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=PT+Serif|Work+Sans&display=swap');
+
 * {
   margin: 0rem;
   padding: 0rem;
   box-sizing: border-box;
+  font-family: 'PT Serif', serif;
+}
+
+p {
+  font-family: 'Work Sans', sans-serif;
+  font-size: 0.8rem;
 }
 </style>
