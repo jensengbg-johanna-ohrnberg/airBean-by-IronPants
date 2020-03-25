b218004d2f1284db0ce178551ed9c8f230922b94
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-25 13:18:52 +0100

    Update App.vue

--- airbean/src/App.vue
+++ airbean/src/App.vue
@@ -22,4 +22,15 @@ p {
   font-family: 'Work Sans', sans-serif;
   font-size: 0.8rem;
 }
+
+.btn {
+  background: #000;
+  padding: 0.6rem 2rem;
+  margin: 1rem 0rem;
+  color: #fff;
+  border: none;
+  border-radius: 3rem;
+  font-size: 1.3rem;
+  font-weight: bold;
+}
 </style>
