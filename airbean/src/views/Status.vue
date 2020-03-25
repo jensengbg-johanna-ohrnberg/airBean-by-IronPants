bae524907a9339241e0ad692f28da50216a8ff7a
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-25 13:18:54 +0100

    Create Status.vue

---
+++ airbean/src/views/Status.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="statusContainer">
+    <p class="orderNum">
+      ordernummer: <strong>{{ orderNum }}</strong>
+    </p>
+    <img src="../assets/graphics/drone.svg" alt="" />
+    <h1>Din beställning är på väg!</h1>
+    <p class="eta">
+      <strong>{{ eta }}</strong> minuter
+    </p>
+    <button class="btn">Ok, coo!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    orderNum() {
+      return this.$store.state.order.orderToSend.orderNumber
+    },
+    eta() {
+      return this.$store.state.order.orderToSend.eta
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.statusContainer {
+  background: #e5674e;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  padding: 4rem;
+  text-align: center;
+
+  strong {
+    font-family: 'Work Sans', sans-serif;
+  }
+
+  .btn {
+    background: #fff;
+    color: #000;
+  }
+
+  .orderNum {
+    color: #fffb;
+  }
+
+  .eta {
+    font-size: 1.4rem;
+  }
+}
+</style>
