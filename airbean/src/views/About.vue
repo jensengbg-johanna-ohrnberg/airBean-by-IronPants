751a9515d0f8e5d64610329ad53678125547aeaa
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-26 11:04:08 +0100

    Update About.vue

--- airbean/src/views/About.vue
+++ airbean/src/views/About.vue
@@ -1,65 +1,48 @@
 <template>
-    <article id='about'>
+  <article id="about">
+    <nav class="main-nav">
+      <MenuButton />
+    </nav>
 
-        <nav class="main-nav">
-            <MenuButton />
-        </nav>
+    <OverlayMenu class="overlay-menu" />
 
-        <OverlayMenu class="overlay-menu"/>
+    <section>
+      <h1 class="title">Vårt kaffe</h1>
 
-        <section>
-            <h1 class="title">Vårt kaffe</h1>
+      <p id="bold" class="paragraph">
+        Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio,
+        grounds dripper, crema, strong whipped, variety extra iced id lungo half
+        and half mazagran. Pumpkin spice.
+      </p>
 
-            <p id="bold" class="paragraph">
-                Pumpkin spice mug,
-                barista cup, sit macchiato,
-                kopi-luwak, doppio,
-                grounds dripper, crema,
-                strong whipped,
-                variety extra iced id lungo half and half mazagran.
-                Pumpkin spice.
-            </p>
+      <p class="paragraph">
+        Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich
+        aftertaste, con panna milk black, arabica white rich beans single shot
+        extra affogato. So affogato macchiato sit extraction instant grinder
+        seasonal organic, turkish single shot, single origin, and robusta strong
+        to go so dripper. Viennese froth, grounds caramelization skinny aromatic
+        cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait
+        flavour cultivar ut bar instant kopi-luwak.
+        <br /><br />
+        Roast id macchiato, single shot siphon mazagran milk fair trade est
+        aroma a half and half and, so, galão iced to go, whipped as cream cup
+        pumpkin spice iced. At extra, rich grinder, brewed to go, steamed half
+        and half at, that, percolator macchiato trifecta and body as arabica
+        dripper. In galão black java milk sit trifecta, robusta, acerbic café au
+        lait instant shop latte. Seasonal bar shop filter aroma id, crema,
+        affogato viennese cultivar aftertaste, seasonal, percolator cream black,
+        galão flavour, milk aromatic turkish skinny crema.
+      </p>
+    </section>
 
-            <p class="paragraph">
-                Que dark fair trade,
-                spoon decaffeinated,
-                barista wings whipped,
-                as rich aftertaste,
-                con panna milk black,
-                arabica white rich beans single shot extra affogato.
-                So affogato macchiato sit extraction instant grinder seasonal organic,
-                turkish single shot,
-                single origin,
-                and robusta strong to go so dripper.
-                Viennese froth,
-                grounds caramelization skinny aromatic cup kopi-luwak,
-                fair trade flavour,
-                frappuccino medium,
-                café au lait flavour cultivar ut bar instant kopi-luwak.
-                <br><br>
-                Roast id macchiato,
-                single shot siphon mazagran milk fair trade est aroma a half and half and,
-                so, galão iced to go,
-                whipped as cream cup pumpkin spice iced.
-                At extra, rich grinder, brewed to go,
-                steamed half and half at, that,
-                percolator macchiato trifecta and body as arabica dripper.
-                In galão black java milk sit trifecta, robusta,
-                acerbic café au lait instant shop latte.
-                Seasonal bar shop filter aroma id,
-                crema, affogato viennese cultivar aftertaste,
-                seasonal, percolator cream black, galão flavour,
-                milk aromatic turkish skinny crema.
-            </p>
-        </section>
-
-        <section>
-            <img class="eva-img" src="../assets/graphics/eva-cortado.jpg" alt="eva-cortado">
-            <h2 class="eva-name">Eva Cortado</h2>
-            <h6 class="eva-info">VD & Grundare</h6>
-        </section>
-
-    </article>
+    <img
+      class="eva-img"
+      src="../assets/graphics/eva-cortado.jpg"
+      alt="eva-cortado"
+    />
+    <h2 class="eva-name">Eva Cortado</h2>
+    <h6 class="eva-info">VD & Grundare</h6>
+  </article>
 </template>
 
 <script>
@@ -76,74 +59,62 @@ export default {
 </script>
 
 <style scoped>
-    .main-nav {
-        position: absolute;
-        display: flex;
-        justify-content: space-between;
-        padding: 0.5rem 0.8rem;
-    }
+.overlay-menu {
+  position: absolute;
+}
 
-    .overlay-menu {
-        position: absolute;
-    }
+#about {
+  position: relative;
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #f3e4e1;
+  background-image: url('../assets/graphics/graphics-header.svg'),
+    url('../assets/graphics/graphics-footer.svg');
+  background-position: top, bottom;
+  background-repeat: no-repeat, no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0rem 2rem;
+}
 
-    #about {
-       position: relative;
-       border: 1px solid black;
-       width: 375px;
-       height: auto;
-       margin: auto;
-       background-color: #F3E4E1;
-       background-image: url("../assets/graphics/graphics-header.svg"), url("../assets/graphics/graphics-footer.svg");
-       background-position: top, bottom;
-       background-repeat: no-repeat, no-repeat;
-   }
+.title {
+  font-size: 42px;
+  font-weight: bold;
+  font-family: 'PT Serif';
+  color: #2f2926;
+  margin-top: 4rem;
+}
 
-   .title {
-       margin-top: 40%;
-       margin-left: 10%;
-       margin-bottom: auto;
-       font-size: 42px;
-       font-weight: bold;
-       font-family: "PT Serif";
-       color: #2F2926;
-   }
+.paragraph {
+  font-size: 14px;
+  font-family: 'Work Sans';
+  color: #2f2926;
+  margin: 2rem 0rem;
+}
 
-   .paragraph {
-       font-size: 14px;
-       font-family: "Work Sans";
-       margin-left: 10%;
-       margin-right: 10%;
-       color: #2F2926;
-   }
+#bold {
+  font-weight: bold;
+}
 
-   #bold {
-       font-weight: bold;
-   }
+.eva-img {
+  border-radius: 50px;
+  margin-top: 3rem;
+}
 
-   .eva-img {
-       border-radius: 50px;
-       margin-top: 5%;
-       margin-left: 38%;
-   }
+.eva-name {
+  color: #2f2926;
+  font-size: 22px;
+  font-weight: bold;
+  font-family: 'PT Serif';
+}
 
-   .eva-name {
-       margin-left: 33%;
-       margin-top: auto;
-       margin-bottom: auto;
-       color: #2F2926;
-       font-size: 22px;
-       font-weight: bold;
-       font-family: "PT Serif";
-   }
-
-   .eva-info {
-       margin-top: auto;
-       margin-left: 41%;
-       margin-bottom: 30%;
-       color: #2F2926;
-       font-family: "Work Sans";
-       font-size: 10px;
-       font-weight: lighter;
-   }
+.eva-info {
+  color: #2f2926;
+  font-family: 'Work Sans';
+  font-size: 10px;
+  font-weight: lighter;
+  margin-bottom: 6rem;
+}
 </style>
