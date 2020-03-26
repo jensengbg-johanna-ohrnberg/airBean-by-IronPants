1e32f56c4a8c6d59a1554389dcc020edcda1ab49
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-26 11:04:04 +0100

    Update MenuButton.vue

--- airbean/src/components/MenuButton.vue
+++ airbean/src/components/MenuButton.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="burger"
-      :class="{ 'active' : isBurgerMenuActive }"
-      @click.prevent="toggle">
-
-        <button type="button" class="burger-button" title="Menu">
-          <img src="../assets/graphics/navicon.svg" alt="navicon">
-        </button>
-
+  <div
+    id="burger"
+    :class="{ active: isBurgerMenuActive }"
+    @click.prevent="toggle"
+  >
+    <img src="../assets/graphics/navicon.svg" alt="navicon" />
   </div>
 </template>
 <script>
@@ -14,53 +12,37 @@ import { store, mutations } from '../store/index'
 
 export default {
   computed: {
-    isBurgerMenuActive () {
+    isBurgerMenuActive() {
       return store.isNavOpen
     }
   },
   methods: {
-    toggle () {
+    toggle() {
       mutations.toggleNav()
     }
   }
 }
 </script>
 
-<style>
-  .hidden {
-    visibility: hidden;
-  }
+<style scoped>
+.hidden {
+  visibility: hidden;
+}
 
-  button {
-    cursor: pointer;
-    margin-top: 12px;
-    margin-left: 6px;
-  }
+button {
+  cursor: pointer;
+  margin-top: 12px;
+  margin-left: 6px;
+  outline: 0;
+}
 
-  button:focus {
-    outline: 0;
-  }
-
-  .burger-button {
-    position: relative;
-    height: 30px;
-    width: 32px;
-    display: block;
-    z-index: 999;
-    border: 0;
-    border-radius: 0;
-    background-color: transparent;
-    pointer-events: all;
-    transition: transform .6s cubic-bezier(.165,.84,.44,1);
-  }
-
-  #burger {
-    background-color: #FFFFFF;
-    border-radius: 50%;
-    width: 50px;
-    height: 50px;
-    margin-top: 5px;
-    margin-left: 0;
-    cursor: pointer;
-  }
+#burger {
+  background-color: #ffffff;
+  border-radius: 50%;
+  padding: 1.4rem 1.3rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
