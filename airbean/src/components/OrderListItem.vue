037edc465c6b6fd69be3c363f326564e4770b751
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-26 15:50:39 +0100

    Create OrderListItem.vue

---
+++ airbean/src/components/OrderListItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <li class="orderItem">
+    <article class="upperLine">
+      <p class="orderNum">6565165535</p>
+      <p>20/03/21</p>
+    </article>
+    <article class="lowerLine">
+      <p>total ordersumma</p>
+      <p>123 kr</p>
+    </article>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'OrderItem'
+}
+</script>
+
+<style lang="scss" scoped>
+.orderItem {
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #ccc;
+  border-bottom: 1px solid #444;
+
+  .orderNum {
+    font-weight: bold;
+  }
+
+  article {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .lowerLine {
+    p {
+      font-size: 0.7rem;
+      color: #999;
+    }
+  }
+}
+</style>
