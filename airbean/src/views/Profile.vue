70165a8b667e6d474b1417c844414590cd444ba9
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-26 15:50:49 +0100

    Update Profile.vue

--- airbean/src/views/Profile.vue
+++ airbean/src/views/Profile.vue
@@ -4,18 +4,32 @@
       <MenuBtn />
     </nav>
     <OverlayMenu />
+    <RegForm />
   </div>
 </template>
 
 <script>
 import MenuBtn from '../components/MenuButton.vue'
 import OverlayMenu from '../components/OverlayMenu'
+import RegForm from '../components/RegForm.vue'
 export default {
   components: {
     MenuBtn,
-    OverlayMenu
+    OverlayMenu,
+    RegForm
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.profileContainer {
+  background: #2f2926;
+  width: 100vw;
+  height: 100vh;
+  padding: 0rem 1rem 1rem 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
