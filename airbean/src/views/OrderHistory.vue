37c4946ddc557da9511e36895be7053a292d943e
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-26 15:50:47 +0100

    Update OrderHistory.vue

--- airbean/src/views/OrderHistory.vue
+++ airbean/src/views/OrderHistory.vue
@@ -1,11 +1,50 @@
 <template>
   <div class="historyContainer">
-    <h2>Your order history</h2>
+    <nav>
+      <MenuBtn />
+    </nav>
+    <OverlayMenu />
+    <img src="../assets/graphics/Profile.svg" alt="" />
+    <article class="userInfo">
+      <h1>Name</h1>
+      <p>Email</p>
+    </article>
+    <OrderList />
   </div>
 </template>
 
 <script>
-export default {}
+import MenuBtn from '../components/MenuButton.vue'
+import OverlayMenu from '../components/OverlayMenu'
+import OrderList from '../components/OrderList'
+export default {
+  components: {
+    MenuBtn,
+    OverlayMenu,
+    OrderList
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.historyContainer {
+  background: #2f2926;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+
+  img {
+    margin-top: 2rem;
+  }
+
+  .userInfo {
+    text-align: center;
+    margin-bottom: auto;
+    margin-top: 0.6rem;
+  }
+}
+</style>
