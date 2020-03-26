c6321d1bb95a40592e4c16a8de2d6943a9fe1a1e
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-26 15:50:36 +0100

    Create OrderList.vue

---
+++ airbean/src/components/OrderList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="orderContainer">
+    <h2>Orderhistorik</h2>
+    <ul class="orderList">
+      <OrderItem />
+      <OrderItem />
+      <OrderItem />
+      <OrderItem />
+      <OrderItem />
+    </ul>
+    <article class="totalSum">
+      <p>Totalt spenderat</p>
+      <p>1225 kr</p>
+    </article>
+  </div>
+</template>
+
+<script>
+import OrderItem from './OrderListItem.vue'
+export default {
+  components: {
+    OrderItem
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.orderContainer {
+  width: 100%;
+  min-height: 20rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 0rem 2rem;
+  margin: 2rem 0rem;
+
+  h2 {
+    margin: 1rem 0rem;
+  }
+
+  ul {
+    list-style: none;
+    width: 100%;
+    margin-bottom: 0;
+  }
+
+  .totalSum {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.6rem 0rem;
+    border-top: 1px solid #fff;
+  }
+}
+</style>
