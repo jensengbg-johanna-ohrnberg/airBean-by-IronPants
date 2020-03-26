427f52f338dd61cf2eb9d948e543c646ea2b5796
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-26 17:32:44 +0100

    Update user.js

--- airbean/src/store/modules/user.js
+++ airbean/src/store/modules/user.js
@@ -1,10 +1,14 @@
 const user = {
   state: {
-    key: ''
+    key: '',
+    res: ''
   },
   mutations: {
     getKey(state, key) {
       state.key = key
+    },
+    sendRes(state, data) {
+      state.res = data
     }
   },
   actions: {
@@ -13,6 +17,20 @@ const user = {
         .then(res => res.json())
         .then(data => {
           ctx.commit('getKey', data)
+        })
+    },
+    createNewUser(ctx, user) {
+      fetch('http://localhost:5000/api/beans/create-user', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user: user })
+      })
+        .then(res => res.json())
+        .then(data => {
+          ctx.commit('sendRes', data.res)
         })
     }
   }
